c520129f06e33c675752b08c27577902d46df5e3
s-blais <stephen@s-blais.com>
2020-12-07 13:48:05 -0500

    add draft create action to TransactionsController

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -4,4 +4,20 @@ class Api::V1::TransactionsController < ApplicationController
     # render json: transactions, include: :fund
     render json: TransactionSerializer.new(transactions)
   end
+
+  def create
+    transaction = Transaction.new(transaction_params)
+    if transaction.save
+      render json: TransactionSerializer.new(transaction), status: :accepted
+    else
+      render json: {errors: transaction.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  transactions_params(params) #these may need adjusting 
+    def params.require(:transaction).permit(:recipient, :contact, :amount :date, :notes, :fund_id)
+    end
+
 end
